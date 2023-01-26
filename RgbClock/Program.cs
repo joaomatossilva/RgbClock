462d3c4822597838e957f2cb092a24a0452a87ad
Joao Matos Silva <joaomatos.silva@farfetch.com>
2023-01-26 23:14:39 +0000

    Hello World Test

--- RgbClock/Program.cs
+++ RgbClock/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RGBLedMatrix;
+
+var matrix = new RGBLedMatrix.RGBLedMatrix(new RGBLedMatrixOptions { ChainLength = 1, Rows = 64, Cols = 64, GpioSlowdown = 4, Brightness = 50 });
+var canvas = matrix.CreateOffscreenCanvas();
+var font = new RGBLedFont("./font.bdf");
+var text = "Hello World!";
+
+canvas.DrawText(font, 1, (10 * 1/*index*/) + 1, new Color(0, 255, 0), text);
+                
+matrix.SwapOnVsync(canvas);
+
+while (!Console.KeyAvailable)
+{
+    Thread.Sleep(250);
+}
