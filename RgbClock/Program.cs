6c58f32055504fa879dc8fb76bee9a6dcdfb2729
Joao Matos Silva <joaomatos.silva@farfetch.com>
2023-01-27 17:57:34 +0000

    Adjusting

--- RgbClock/Program.cs
+++ RgbClock/Program.cs
@@ -7,13 +7,15 @@ var font = new RGBLedFont("./font.bdf");
 var date = DateTime.Now;
 
 var day = date.ToString("dd-MM");
-canvas.DrawText(font, 1, (10 * 1/*index*/) + 1, new Color(0, 255, 0), day);
+canvas.DrawText(font, 1, (10 * 1/*index*/) + 1, new Color(120, 120, 120), day);
 
-var hour = date.ToString("HH-mm");
-canvas.DrawText(font, 1 + 25 + 3, (10 * 1/*index*/) + 1, new Color(0, 255, 0), hour);
+var hour = date.ToString("HH:mm");
+canvas.DrawText(font, 1 + 20 + 3, (10 * 1/*index*/) + 1, new Color(235, 235, 235), hour);
 
 var dayOfWeek = date.ToString("ddd");
-canvas.DrawText(font, 1 + 25 + 3 + 25 + 3, (10 * 1/*index*/) + 1, new Color(0, 255, 0), dayOfWeek);
+canvas.DrawText(font, 1 + 20 + 3 + 20 + 3, (10 * 1/*index*/) + 1, new Color(255, 120, 120), dayOfWeek);
+
+canvas.DrawLine(1, 13, 62, 13, new Color(50, 50, 50));
                 
 matrix.SwapOnVsync(canvas);
 
