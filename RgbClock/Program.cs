23512ebb09f9eed9bb085ac00c554a738dc19f70
Joao Matos Silva <joaomatos.silva@farfetch.com>
2023-01-27 00:08:05 +0000

    datetime now

--- RgbClock/Program.cs
+++ RgbClock/Program.cs
@@ -3,9 +3,17 @@
 var matrix = new RGBLedMatrix.RGBLedMatrix(new RGBLedMatrixOptions { ChainLength = 1, Rows = 64, Cols = 64, GpioSlowdown = 4, Brightness = 50 });
 var canvas = matrix.CreateOffscreenCanvas();
 var font = new RGBLedFont("./font.bdf");
-var text = "Hello World!";
 
-canvas.DrawText(font, 1, (10 * 1/*index*/) + 1, new Color(0, 255, 0), text);
+var date = DateTime.Now;
+
+var day = date.ToString("dd-MM");
+canvas.DrawText(font, 1, (10 * 1/*index*/) + 1, new Color(0, 255, 0), day);
+
+var hour = date.ToString("HH-mm");
+canvas.DrawText(font, 1 + 25 + 3, (10 * 1/*index*/) + 1, new Color(0, 255, 0), hour);
+
+var dayOfWeek = date.ToString("ddd");
+canvas.DrawText(font, 1 + 25 + 3 + 25 + 3, (10 * 1/*index*/) + 1, new Color(0, 255, 0), dayOfWeek);
                 
 matrix.SwapOnVsync(canvas);
 
